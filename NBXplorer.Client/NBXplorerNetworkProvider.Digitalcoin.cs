5e58058d6303d00db14c9e182cc7d9300acc8505
Alex <a.adam@wewanttoknow.com>
2018-07-12 15:41:52 +0200

    Add Digitalcoin DGC

---
+++ NBXplorer.Client/NBXplorerNetworkProvider.Digitalcoin.cs
@@ -0,0 +1,23 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBXplorer
+{
+    public partial class NBXplorerNetworkProvider
+    {
+		private void InitDigitalcoin(NetworkType networkType)
+		{
+			Add(new NBXplorerNetwork(NBitcoin.Altcoins.Digitalcoin.Instance, networkType)
+			{
+				MinRPCVersion = 120000
+			});
+		}
+
+		public NBXplorerNetwork GetLTC()
+		{
+			return GetFromCryptoCode(NBitcoin.Altcoins.Digitalcoin.Instance.CryptoCode);
+		}
+	}
+}
--- NBXplorer.Client/NBXplorerNetworkProvider.cs
+++ NBXplorer.Client/NBXplorerNetworkProvider.cs
@@ -21,6 +21,7 @@ namespace NBXplorer
 			InitFeathercoin(networkType);
 			InitUfo(networkType);
 			InitViacoin(networkType);
+			InitDigitalcoin(networkType);
 			NetworkType = networkType;
 			foreach(var chain in _Networks.Values)
 			{
